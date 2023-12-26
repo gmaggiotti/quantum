ee6a4ebc6e91e573ebf2ff8bcc3cc4b28132b2c6
gabe <gmaggiotti@gmail.com>
2023-12-25 20:43:23 -0500

    first commit of the quantum project

---
+++ src/bells-inequality.py
@@ -0,0 +1,14 @@
+# General
+import numpy as np
+
+# Qiskit imports
+from qiskit import QuantumCircuit
+from qiskit.circuit import Parameter
+from qiskit.quantum_info import SparsePauliOp
+
+# Qiskit Runtime imports
+from qiskit_ibm_runtime import QiskitRuntimeService, Estimator, Batch
+
+# Plotting routines
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tck
