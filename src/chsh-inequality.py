aeebcb6d8bdaa9f2cf5d2a598c0dd2781fae9efa
gabe <gmaggiotti@gmail.com>
2023-12-26 12:41:46 -0500

    first commit of the quantum project

--- src/chsh-inequality.py
+++ src/chsh-inequality.py
@@ -14,3 +14,85 @@ from qiskit_ibm_runtime import QiskitRuntimeService, Estimator, Batch
 # Plotting routines
 import matplotlib.pyplot as plt
 import matplotlib.ticker as tck
+
+token = "f8da5f44ab6afeb23251e443164264b3bfac9314213627d2c7ab1e39dd568f367b6ed9044b6e067ce11da34cfa06db83d22012a6a04465572bdc12ac3460914f"
+
+# To run on hardware, select the backend with the fewest number of jobs in the queue
+service = QiskitRuntimeService(channel="ibm_quantum", token=token)
+backend = service.least_busy(operational=True, simulator=False)
+print(backend.name)
+
+theta = Parameter("$\\theta$")
+
+chsh_circuit_no_meas = QuantumCircuit(2)
+chsh_circuit_no_meas.h(0)
+chsh_circuit_no_meas.cx(0, 1)
+chsh_circuit_no_meas.ry(theta, 0)
+chsh_circuit_no_meas.draw(output="mpl", idle_wires=False, style="iqp")
+
+number_of_phases = 21
+phases = np.linspace(0, 2 * np.pi, number_of_phases)
+# Phases need to be expressed as list of lists in order to work
+individual_phases = [[ph] for ph in phases]
+
+ZZ = SparsePauliOp.from_list([("ZZ", 1)])
+ZX = SparsePauliOp.from_list([("ZX", 1)])
+XZ = SparsePauliOp.from_list([("XZ", 1)])
+XX = SparsePauliOp.from_list([("XX", 1)])
+
+ops = [ZZ, ZX, XZ, XX]
+num_ops = len(ops)
+
+batch_circuits = [chsh_circuit_no_meas] * number_of_phases * num_ops
+batch_ops = [op for op in ops for _ in individual_phases]
+
+# To run on a local simulator:
+#   1. Use the Estimator from qiskit.primitives instead.
+#   2. Remove the Batch context manager below.
+
+with Batch(backend=backend) as batch:
+    estimator = Estimator()
+
+    batch_expvals = (
+        estimator.run(
+            batch_circuits, batch_ops, parameter_values=individual_phases * num_ops, shots=10000
+        )
+        .result()
+        .values
+    )
+
+ZZ_expval, ZX_expval, XZ_expval, XX_expval = [
+    batch_expvals[kk * number_of_phases: (kk + 1) * number_of_phases] for kk in range(num_ops)
+]
+
+# <CHSH1> = <AB> - <Ab> + <aB> + <ab>
+chsh1_est = ZZ_expval - ZX_expval + XZ_expval + XX_expval
+
+# <CHSH2> = <AB> + <Ab> - <aB> + <ab>
+chsh2_est = ZZ_expval + ZX_expval - XZ_expval + XX_expval
+
+fig, ax = plt.subplots(figsize=(10, 6))
+
+# results from hardware
+ax.plot(phases / np.pi, chsh1_est, "o-", label="CHSH1", zorder=3)
+ax.plot(phases / np.pi, chsh2_est, "o-", label="CHSH2", zorder=3)
+
+# classical bound +-2
+ax.axhline(y=2, color="0.9", linestyle="--")
+ax.axhline(y=-2, color="0.9", linestyle="--")
+
+# quantum bound, +-2√2
+ax.axhline(y=np.sqrt(2) * 2, color="0.9", linestyle="-.")
+ax.axhline(y=-np.sqrt(2) * 2, color="0.9", linestyle="-.")
+ax.fill_between(phases / np.pi, 2, 2 * np.sqrt(2), color="0.6", alpha=0.7)
+ax.fill_between(phases / np.pi, -2, -2 * np.sqrt(2), color="0.6", alpha=0.7)
+
+# set x tick labels to the unit of pi
+ax.xaxis.set_major_formatter(tck.FormatStrFormatter("%g $\pi$"))
+ax.xaxis.set_major_locator(tck.MultipleLocator(base=0.5))
+
+# set labels, and legend
+plt.xlabel("Theta")
+plt.ylabel("CHSH witness")
+plt.legend()
+plt.show()
