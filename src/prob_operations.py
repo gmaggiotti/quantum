134bdb9080e921499795e10d643f98145b5a6d93
gabe <gmaggiotti@gmail.com>
2024-09-28 12:43:40 -0400

    plus comment

--- src/prob_operations.py
+++ src/prob_operations.py
@@ -10,6 +10,9 @@ a = np.array([[0, 1]])
 result = np.matmul(f_a, a)
 print(result)
 
+# The example of a qubit state vector above is very commonly encountered —
+# it is called the plus state and is denoted as follows:
+# |+⟩ = √2/2 |0⟩ + √2/2 |1⟩
 plus_state = np.array([1 / math.sqrt(2), 1 / math.sqrt(2)])
 print(plus_state[0])
 minus_state = np.array([1 / math.sqrt(2), -1 / math.sqrt(2)])
