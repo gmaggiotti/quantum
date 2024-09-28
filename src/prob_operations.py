cb8a61b1c3b441b2bbd4e35afd174475a55a9984
gabe <gmaggiotti@gmail.com>
2024-09-28 10:59:17 -0400

    add minus and plus states

--- src/prob_operations.py
+++ src/prob_operations.py
@@ -1,9 +1,17 @@
+import math
+
 import numpy as np
 
 # M = ∑ ∣f(a)⟩⟨a∣
 #     a∈Σ
 
-f_a = np.array([[1/2,1/2]]).T
-a = np.array([[0,1]])
-result = np.matmul(f_a,a)
+f_a = np.array([[1 / 2, 1 / 2]]).T
+a = np.array([[0, 1]])
+result = np.matmul(f_a, a)
 print(result)
+
+plus_state = np.array([1 / math.sqrt(2), 1 / math.sqrt(2)])
+print(plus_state[0])
+minus_state = np.array([1 / math.sqrt(2), -1 / math.sqrt(2)])
+
+print(np.dot(plus_state, minus_state))
