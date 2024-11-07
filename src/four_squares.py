3a3d5eff7146608e1f5e1644dee53317849d4413
gabe <gmaggiotti@gmail.com>
2024-11-06 21:05:09 -0500

    fixing import problems

--- src/four_squares.py
+++ src/four_squares.py
@@ -1,6 +1,5 @@
 from qiskit import QuantumCircuit
 import json
-from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2 as Sampler
 
 # Load key
 def load_key(filename):
@@ -16,35 +15,34 @@ num_qubits = 10
 # create the circuit
 
 four_squares = QuantumCircuit(num_qubits, num_qubits)
-four_squares.h([0,1,2,3])
-four_squares.barrier([4,5])
-four_squares.h([4,5])
-four_squares.barrier(range(4,7))
-four_squares.cx([1,3],6)
-four_squares.barrier([6,7])
-four_squares.cx([2,3], 7)
-four_squares.barrier([6,7])
-four_squares.cx(4,6)
-four_squares.cx(5,7)
-four_squares.ccx(6,7,3)
+four_squares.h([0, 1, 2, 3])
+four_squares.barrier([4, 5])
+four_squares.h([4, 5])
+four_squares.barrier(range(4, 7))
+four_squares.cx([1, 3], 6)
+four_squares.barrier([6, 7])
+four_squares.cx([2, 3], 7)
+four_squares.barrier([6, 7])
+four_squares.cx(4, 6)
+four_squares.cx(5, 7)
+four_squares.ccx(6, 7, 3)
 four_squares.x(6)
-four_squares.ccx(6,7,2)
+four_squares.ccx(6, 7, 2)
 four_squares.x(6)
-four_squares.barrier([6,7])
+four_squares.barrier([6, 7])
 
 four_squares.x(7)
-four_squares.ccx(6,7,1)
+four_squares.ccx(6, 7, 1)
 four_squares.x(7)
-four_squares.barrier([6,7])
+four_squares.barrier([6, 7])
 four_squares.x(6)
 four_squares.x(7)
-four_squares.ccx(6,7,0)
+four_squares.ccx(6, 7, 0)
 four_squares.x(6)
 four_squares.x(7)
-four_squares.barrier([0,9])
-four_squares.cx([1,3],8)
-four_squares.cx([2,3],8)
-
+four_squares.barrier([0, 9])
+four_squares.cx([1, 3], 8)
+four_squares.cx([2, 3], 8)
 
 four_squares.draw('mpl')
 print(four_squares)
--- src/tsp.py
+++ src/tsp.py
@@ -1,33 +1,38 @@
- # conceptual implementation using quantum computing for solving a logistic problem.
- # This example uses a quantum-inspired algorithm for the Traveling Salesman Problem (TSP)
- # which can be adapted for more complex logistic problems
+# conceptual implementation using quantum computing for solving a logistic problem.
+# This example uses a quantum-inspired algorithm for the Traveling Salesman Problem (TSP)
+# which can be adapted for more complex logistic problems
 
-
-from qiskit import Aer, QuantumCircuit, execute
-from qiskit.optimization.applications.ising import tsp
-from qiskit.optimization.applications.ising.common import sample_most_likely
+from qiskit_aer import Aer
+from qiskit_optimization import QuadraticProgram
+from qiskit_optimization.applications import tsp as tsp_ising
+from qiskit_optimization.algorithms import warm_start_qaoa_optimizer
+from qiskit_optimization.applications.optimization_application import COBYLA
+from qiskit.utils import QuantumInstance
 import numpy as np
 
-# Define the TSP problem with distance matrix
-n = 4  # number of cities
-num_qubits = n**2
+# Define the number of cities and create a random distance matrix
+n = 4  # Number of cities
 distance_matrix = np.random.randint(1, 10, size=(n, n))
 
-# Create a quantum circuit for TSP
-tsp_circuit = tsp.get_tsp_qubitops(distance_matrix)
-qubit_op, offset = tsp_circuit
+# Make sure the distance matrix is symmetric (since TSP is undirected)
+distance_matrix = (distance_matrix + distance_matrix.T) // 2
 
-# Solve using Quantum Approximate Optimization Algorithm (QAOA)
-from qiskit.aqua.algorithms import QAOA
-from qiskit.aqua.components.optimizers import COBYLA
+# Convert the distance matrix into the Ising formulation (QUBO)
+qubo, offset = tsp_ising.get_operator(distance_matrix)
 
-qaoa = QAOA(qubit_op, COBYLA(), p=3)
+# Set up the QAOA algorithm with the QUBO
+qaoa = QAOA(qubo, COBYLA(), p=3)
+
+# Set up the quantum instance using a statevector simulator
 backend = Aer.get_backend('statevector_simulator')
 quantum_instance = QuantumInstance(backend, shots=1024)
 
+# Run the algorithm to get the result
 result = qaoa.run(quantum_instance)
-x = sample_most_likely(result.eigenstate)
 
-# Extract the solution
-solution = tsp.get_tsp_solution(x)
+# Extract the solution (most likely bitstring)
+x = result['x']  # This is the binary solution vector
+
+# Decode the solution into a TSP tour
+solution = tsp_ising.get_tsp_solution(x)
 print("Optimal tour:", solution)
