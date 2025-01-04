5a09c1dfbfa0e4c6aea644fa929d7ac31177073b
gabe <gmaggiotti@gmail.com>
2025-01-03 20:20:11 -0500

    qc operators different implementations

--- src/qc_operators_imp.py
+++ src/qc_operators_imp.py
@@ -1,6 +1,8 @@
 import numpy as np
 from math import sqrt
 
+from qiskit.visualization import plot_histogram
+
 # Define basic quantum gates
 X = np.array([[0, 1], [1, 0]])
 H = (1 / np.sqrt(2)) * np.array([[1, 1], [1, -1]])
@@ -52,4 +54,53 @@ measurement_result = measure(state_after_CNOT)
 print("State after Hadamard and CNOT: ", state_after_CNOT)
 print("Measurement result: ", measurement_result)
 
-apply_S()
+#  Performing operations with Operator and Statevector
+from qiskit.quantum_info import Operator, Statevector
+
+X = Operator([[0, 1], [1, 0]])
+Y = Operator([[0, -1.0j], [1.0j, 0]])
+Z = Operator([[1, 0], [0, -1]])
+H = Operator([[1 / sqrt(2), 1 / sqrt(2)], [1 / sqrt(2), -1 / sqrt(2)]])
+S = Operator([[1, 0], [0, 1.0j]])
+T = Operator([[1, 0], [0, (1 + 1.0j) / sqrt(2)]])
+
+v = Statevector([1, 0])
+
+v = v.evolve(H)
+v = v.evolve(T)
+v = v.evolve(H)
+v = v.evolve(T)
+v = v.evolve(Z)
+
+print(v.draw("text"))
+
+from qiskit import QuantumCircuit
+
+circuit = QuantumCircuit(1)
+
+circuit.h(0)
+circuit.t(0)
+circuit.h(0)
+circuit.t(0)
+circuit.z(0)
+
+circuit.draw()
+
+# simulate the result of running this circuit
+ket0 =Statevector([1, 0])
+v = ket0.evolve(circuit)
+statistics = v.sample_counts(4000)
+plot_histogram(statistics)
+
+# simulate same result using matrix multiplication
+ket0 = np.array([1, 0])
+aux = np.matmul(H, ket0)
+aux = np.matmul(T, aux)
+aux = np.matmul(H, aux)
+aux = np.matmul(T, aux)
+aux = np.matmul(Z, aux)
+
+print("state probabilities", aux)
+print("colapsed into a classical state",measure(aux))
+print("EOL")
+
