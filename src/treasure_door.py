0000b557738982da33fc8253a1292690f77df5f1
gabe <gmaggiotti@gmail.com>
2025-01-04 08:24:08 -0500

    plting histogram

--- src/treasure_door.py
+++ src/treasure_door.py
@@ -1,5 +1,7 @@
 from qiskit import QuantumCircuit, transpile
+from qiskit.quantum_info import Statevector
 from qiskit_aer import AerSimulator
+from qiskit.visualization import plot_histogram
 import json
 
 # Plotting routines
@@ -34,15 +36,26 @@ treasure_door.cx(2, 1)
 treasure_door.x(2)
 treasure_door.cx(2, 0)
 treasure_door.x(2)
-treasure_door.measure([0, 1, 2], [0, 1, 2])
+# treasure_door.measure([0, 1, 2], [0, 1, 2])
 treasure_door.draw(output="mpl", idle_wires=False, style="iqp")
 print(treasure_door)
 
 # Run the circuit
-simulator = AerSimulator()
-qc_compiled = transpile(treasure_door, simulator)
-job = simulator.run(qc_compiled, shots=1024)
-result = job.result()
+classical_state = '100'
+state = Statevector.from_label(classical_state)
+v =state.evolve(treasure_door)
 
-# Print the probabilities
-print(result.get_counts(qc_compiled))
+samples = 4000
+statistics = v.sample_counts(samples)
+print(statistics)
+plot_histogram(statistics)
+
+
+print("EOF")
+# simulator = AerSimulator()
+# qc_compiled = transpile(treasure_door, simulator)
+# job = simulator.run(qc_compiled, shots=1024)
+# result = job.result()
+#
+# # Print the probabilities
+# print(result.get_counts(qc_compiled))
